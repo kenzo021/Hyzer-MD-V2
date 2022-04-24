e5c75f7129268e427c1da371f8b136a33b6bb8f0
kenzo021 <92974890+kenzo021@users.noreply.github.com>
2022-04-24 21:10:58 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,10 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Sewa Bot 〙 ═
-╠➥ *1 Minggu* : Rp 5.000
-╠➥ *2 Minggu* : Rp 10.000
-╠➥ *3 Minggu* : Rp 15.000
-╠➥ *1 Bulan*   : Rp 20.000
+╠➥ *1 Minggu* : Rp 5.000,00
+╠➥ *3 Minggu* : Rp 14.000,00
+╠➥ *1 Bulan*   : Rp 18.000,00
 ║
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
 ╠➥ Ovo & Dana
