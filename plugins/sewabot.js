8c22a2e065948320e6d5bd22cf726d76ac7bbb21
kenzo021 <92974890+kenzo021@users.noreply.github.com>
2022-05-20 09:13:58 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,16 +1,39 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Sewa Bot 〙 ═
-╠➥ *1 Minggu* : Rp 5.000,00
-╠➥ *3 Minggu* : Rp 14.000,00
-╠➥ *1 Bulan*   : Rp 18.000,00
-║
-╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
-╠➥ Ovo & Dana
-║
-╠═ Tertarik untuk menyewa Bot Ini?
-╠➥ Hubungi Owner ➡️ https://wa.me/6285251626154?text=Permisi+mau+sewa+bot
-║
+	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `                「 Xylrnd BOT 」
+
+╭━━━━「 *SEWA* 」
+┊⫹⫺ *Hemat:* _5k/grup (1 minggu)_
+┊⫹⫺ *Normal:* _15k/grup (1 bulan)_
+┊⫹⫺ *Standar:* _30k/grup (2 bulan)_
+┊⫹⫺ *Pro:* _35k/grup (4 bulan)_                                                      
+┊⫹⫺ *Vip:* = _65k/grup (12 bulan)_
+╰═┅═━––––––๑
+
+╭━━━━「 *PREMIUM* 」
+┊⫹⫺ *Hemat:* _5k (1 minggu)_
+┊⫹⫺ *Normal:* _20k (1 bulan)_
+┊⫹⫺ *Pro:* _40k (4 bulan)_
+┊⫹⫺ *Vip:* _50k (8 bulan)_                                               
+┊⫹⫺ *Permanent:* = _70k (Unlimited)_
+╰═┅═━––––––๑
+
+*⫹⫺ PAYMENT:*
+• *Pulsa:* [085251626154]
+• *Dana:* [085251626154]
+• *OVO:* [085251626154]
+• *Gopay:* [085251626154]
+
+–––––– *🐾 Kebijakan* ––––––
+🗣️: Kak, Kok harganya mahal banget?
+💬: Mau tawar menawar? boleh, silahkan chat owner aja
+
+🗣️: Scam ga nih kak?
+💬: Enggalah, Owner 100% Tepati janji #STAYHALAL
+
+Tertarik untuk menyewa Bot Ini?
+➥ Hubungi Owner ➡️ https://wa.me/6285251626154?text=Permisi+mau+sewa+bot
+
 ╠═〘 Xylrnd-Bot 〙 ═`.trim(), wm, 'Ovo', '.viaovo', 'Dana', '.viadana', m)
 }
 
